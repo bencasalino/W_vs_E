e38f3531546015c3ca3a7d1e89ce111232053ad8
Ben Casalino <bencasalino@gmail.com>
2017-09-27 12:10:21 -0600

    jquery workin

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,25 @@
-console.log("yooooo");
+// console.log("yooooo");
+
+
+
+
+
+
+$(document).ready(function() {
+    $("#hide").click(function() {
+        $("#show-this-box").hide();
+    });
+    $("#show").click(function() {
+        $("#show-this-box").show();
+    });
+});
+
+
+// $(document).ready(function() {
+//     $("#phx_hide").click(function() {
+//         $("#two").hide();
+//     });
+//     $("#phx_show").click(function() {
+//         $("#two").show();
+//     });
+// });
